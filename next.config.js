37a8d13c3db4e653b977d2db6e34c1de27007dc0
Anurag-Bharati <anuragbharati26@gmail.com>
2023-03-09 22:31:24 +0545

    Configured svgr and dependencies. Added general styles

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/", "styles")],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
 module.exports = nextConfig;
