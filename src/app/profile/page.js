6b0e0366a1fc5894e1e4149983f870321c6da7f3
Anurag-Bharati <anuragbharati26@gmail.com>
2023-05-11 23:57:17 +0545

    Added my-orders and my-reservation section on the profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,11 +1,15 @@
 import Navbar from "@/components/landing/Navbar";
 import Hero from "@/components/profile/Hero";
+import MyOrders from "@/components/profile/MyOrders";
+import MyReservations from "@/components/profile/MyReservations";
 
 export default function Profile() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <MyOrders />
+      <MyReservations />
     </main>
   );
 }
---
+++ src/components/profile/MyOrders.jsx
@@ -0,0 +1,91 @@
+"use client";
+
+import ContentCard from "./MyOrderCard";
+import ReactPaginate from "react-paginate";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { firestore } from "@/config/firebase/firebase";
+import { useSession } from "next-auth/react";
+import { useEffect, useMemo, useState } from "react";
+import { DummyContentCard } from "./DummyCard";
+
+const itemsPerPage = 3;
+
+const MyOrders = () => {
+  const { data: session, status } = useSession({ required: true });
+  const uid = session?.user?.id;
+  const [myOrders, setMyOrders] = useState([]);
+  const collectionRef = useMemo(() => collection(firestore, "orders"), []);
+
+  useEffect(() => {
+    if (status !== "authenticated" || !uid) return;
+    const q = query(collectionRef, where("user.id", "==", uid), orderBy("createdAt", "desc"));
+    getDocs(q).then((querySnapshot) => {
+      if (querySnapshot.empty) return;
+      setMyOrders(querySnapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })));
+    });
+  }, [collectionRef, status, uid]);
+
+  // Pagination
+  const [itemOffset, setItemOffset] = useState(0);
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = myOrders.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(myOrders.length / itemsPerPage);
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % myOrders.length;
+    setItemOffset(newOffset);
+  };
+  return (
+    <section
+      className="w-full relative isolate scroll-mt-10 md:scroll-mt-20  md:py-8 lg:py-12 overflow-hidden px-[5]"
+      id="my-orders"
+    >
+      <header className=" relative mx-auto max-w-3xl w-full  md:pl-2 md:pb-4 flex-col sm:flex-row flex justify-between items-center md:h-9">
+        <h1 className="text-2xl align-middle mb-3 sm:mb-0">My Orders</h1>
+        <div className="flex gap-2 items-center flex-wrap justify-center pb-2 sm:pb-0">
+          {myOrders.length === 0 && (
+            <>
+              <span className="h-6 w-32 md:w-64 rounded-full shimmer" />
+              <span className="h-6 w-7 rounded-full shimmer" />
+              <span className="h-6 w-7 rounded-full shimmer" />
+            </>
+          )}
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={1}
+            pageCount={pageCount}
+            previousLabel="< previous"
+            renderOnZeroPageCount={null}
+            marginPagesDisplayed={1}
+          />
+        </div>
+      </header>
+
+      <div className="mx-auto max-w-3xl w-full bg-zinc-100 p-2 md:p-4 rounded-xl">
+        <div className="flex flex-col gap-2">
+          {myOrders.length === 0 && (
+            <>
+              <DummyContentCard />
+              <DummyContentCard />
+              <DummyContentCard />
+            </>
+          )}
+          <Orders currentItems={currentItems} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MyOrders;
+
+function Orders({ currentItems }) {
+  return (
+    <>
+      {currentItems.map((d, i) => (
+        <ContentCard i={i} data={d} key={i} />
+      ))}
+    </>
+  );
+}
